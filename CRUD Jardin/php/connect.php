37e1984c2131d63a4bacd33629e9301e33c9bdb6
ManuelGallego1 <107779249+ManuelGallego1@users.noreply.github.com>
2023-04-25 16:30:46 -0500

    Delete CRUD Jardin directory

--- CRUD Jardin/php/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "root", "control22", "db_jardin");
-if ($mysqli->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-
